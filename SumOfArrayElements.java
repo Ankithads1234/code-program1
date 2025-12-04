b6d3404488dded37fc816f779600428ab2e1ebb2
Ankithads1234 <ankithads51@gmail.com>
2025-12-04 18:50:24 +0530

    Add files via upload

---
+++ SumOfArrayElements.java
@@ -0,0 +1,18 @@
+package Com.acharya.classes;
+
+public class SumOfArrayElements {
+
+	public static void main(String[] args) {
+		int [] arr= {12,21,11,43};
+		int sum=0;
+		System.out.println("array values");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]+"");
+		sum+=arr[i];
+	}
+	System.out.println("sum of value="+sum);s
+
+	}
+
+}
