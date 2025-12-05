cb5dd0e84212a10fdf5d7c53b92f28f39e9c6f4a
Ankithads1234 <ankithads51@gmail.com>
2025-12-05 19:07:06 +0530

    Add files via upload

---
+++ PrimeOrNot.java
@@ -0,0 +1,28 @@
+package Com.acharya.classes;
+
+import java.util.Scanner;
+
+public class PrimeOrNot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int num = sc.nextInt();
+
+		int count = 0;
+
+		for (int i = 1; i <= num; i++) {
+			if (num % i == 0) {
+				count++;
+			}
+		}
+
+		if (count == 2) {
+			System.out.println("Prime number");
+		} else {
+			System.out.println("Not a prime number");
+		}
+	}
+}
