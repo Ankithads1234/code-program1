394c0354b36bf955c853e27f2f84b641f7d40784
Ankithads1234 <ankithads51@gmail.com>
2025-12-05 20:50:25 +0530

    Add files via upload

--- FirstOrLastElement.java
+++ FirstOrLastElement.java
@@ -2,22 +2,20 @@ package Com.acharya.classes;
 
 public class FirstOrLastElement {
 
-public static void main(String[] args) {
-        
-        int[] arr = {6, 2, 3, 4};  
-        
+	public static void main(String[] args) {
+
+        int[] arr = {6, 3, 4, 5};   
+
         
         if (arr.length >= 1) {
 
            
-            if (arr[0] == 6 || arr[arr.length - 1] == 6) {
-                System.out.println(true);
-            } else {
-                System.out.println(false);
-            }
+            boolean result = (arr[0] == 6 || arr[arr.length - 1] == 6);
+
+            System.out.println(result);
 
         } else {
-            System.out.println("Array length is less than 1");
+            System.out.println("Array length must be 1 or more");
         }
     }
 }
