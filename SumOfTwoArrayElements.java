615cdc521e930fd3cd2ccc1dbef38505f746ab0c
Ankithads1234 <ankithads51@gmail.com>
2025-12-08 19:07:57 +0530

    Add files via upload

---
+++ SumOfTwoArrayElements.java
@@ -0,0 +1,25 @@
+package Com.acharya.classes;
+import java.util.Scanner;
+public class SumOfTwoArrayElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+          int arr[][]=new int[2][3];
+          for(int i=0;i<arr.length;i++) {
+        	  for(int j=0;j<arr[i].length;j++) {
+        		  System.out.println("enter the value for row"+(i+1)+"abd column"+(j+1));
+        		  arr[i][j]=sc.nextInt();
+        		  
+        	  }
+          }
+          for(int i=0;i<arr.length;i++) {
+        	  for(int j=0;j<arr[i].length;j++) {
+        		  System.out.println(arr[i][j]+"");
+        	  }System.out.println();
+        	  }
+          }
+          
+	}
+
+
